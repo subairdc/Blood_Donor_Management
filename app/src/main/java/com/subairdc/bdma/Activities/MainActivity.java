bb19d206fc176a5f48c134f0319858853dbbabd2
subairdc <subairdc@gmail.com>
2021-09-11 09:03:07 +0530

    Create Login page(MainActivity) and UserRegister page

--- app/src/main/java/com/subairdc/bdma/Activities/MainActivity.java
+++ app/src/main/java/com/subairdc/bdma/Activities/MainActivity.java
@@ -2,15 +2,33 @@ package com.subairdc.bdma.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.subairdc.bdma.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
+
     }
 }
---
+++ app/src/main/java/com/subairdc/bdma/Activities/RegisterUser.java
@@ -0,0 +1,16 @@
+package com.subairdc.bdma.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.subairdc.bdma.R;
+
+public class RegisterUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+}
