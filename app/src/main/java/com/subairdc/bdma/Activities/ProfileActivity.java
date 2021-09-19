5f25035850d16c84a0c356c0292803915476bbf2
subairdc <subairdc@gmail.com>
2021-09-19 20:58:16 +0530

    Add logout button

--- app/src/main/java/com/subairdc/bdma/Activities/ProfileActivity.java
+++ app/src/main/java/com/subairdc/bdma/Activities/ProfileActivity.java
@@ -2,15 +2,32 @@ package com.subairdc.bdma.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.subairdc.bdma.R;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logout = (Button) findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            }
+        });
+
     }
 }
