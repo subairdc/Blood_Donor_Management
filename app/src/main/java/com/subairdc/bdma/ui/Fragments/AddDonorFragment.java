ec777a7dbed6c3b2d3440118ddfb8d0ffff0d29b
subairdc <subairdc@gmail.com>
2021-11-12 09:00:19 +0530

    Add Donor Fragment

---
+++ app/src/main/java/com/subairdc/bdma/ui/Fragments/AddDonorFragment.java
@@ -0,0 +1,66 @@
+package com.subairdc.bdma.ui.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.subairdc.bdma.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AddDonorFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AddDonorFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public AddDonorFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AddDonorFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AddDonorFragment newInstance(String param1, String param2) {
+        AddDonorFragment fragment = new AddDonorFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_donor, container, false);
+    }
+}
