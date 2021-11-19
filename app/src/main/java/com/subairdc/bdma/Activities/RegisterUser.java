12773402bceabbcab64a3f3ebe774964886e3e4a
subairdc <subairdc@gmail.com>
2021-11-19 18:41:29 +0530

    Add Binding and add gender,DOB

--- app/src/main/java/com/subairdc/bdma/Activities/RegisterUser.java
+++ app/src/main/java/com/subairdc/bdma/Activities/RegisterUser.java
@@ -3,11 +3,15 @@ package com.subairdc.bdma.Activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -20,29 +24,39 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.subairdc.bdma.R;
+import com.subairdc.bdma.databinding.ActivityRegisterUserBinding;
+
+import java.util.Calendar;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
 
     private TextView bannar, registerUser;
     private EditText editTextName, editTextPhoneNo, editTextEmail, editTextPassword, editTextConfirmPassword;
     private ProgressBar progressBar;
+    DatePickerDialog.OnDateSetListener setListener;
+
+    ActivityRegisterUserBinding binding;
+
 
     private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_user);
+        //setContentView(R.layout.activity_register_user);
+        binding = ActivityRegisterUserBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         mAuth = FirebaseAuth.getInstance();
 
-        bannar = (TextView) findViewById(R.id.bannar);
-        bannar.setOnClickListener(this);
+        //bannar = (TextView) findViewById(R.id.bannar);
+        binding.bannar.setOnClickListener(this);
 
-        registerUser = (Button) findViewById(R.id.registerUser);
-        registerUser.setOnClickListener(this);
+        //registerUser = (Button) findViewById(R.id.registerUser);
+        binding.registerUser.setOnClickListener(this);
 
-        editTextName = (EditText) findViewById(R.id.nameTitle);
+        //editTextName = (EditText) findViewById(R.id.nameTitle);
         editTextPhoneNo = (EditText) findViewById(R.id.phoneNo);
         editTextEmail = (EditText) findViewById(R.id.email);
         editTextPassword = (EditText) findViewById(R.id.password);
@@ -50,6 +64,26 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        binding.dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        RegisterUser.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        month = month+1;
+                        String date = day+"/"+month+"/"+year;
+                        binding.dob.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
     }
 
     @Override
@@ -65,57 +99,75 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
     }
 
     private void registerUser() {
-        String name = editTextName.getText().toString().trim();
-        String phoneNo = editTextPhoneNo.getText().toString().trim();
-        String email = editTextEmail.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
-        String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+        String name = binding.name.getText().toString().trim();
+        String gender = binding.gender.toString().trim();
+        String dob = binding.dob.getText().toString().trim();
+        String phoneNo = binding.phoneNo.getText().toString().trim();
+        String email = binding.email.getText().toString().trim();
+        String password = binding.password.getText().toString().trim();
+        String confirmPassword = binding.confirmPassword.getText().toString().trim();
 
         if(name.isEmpty()){
-            editTextName.setError("Full Name is required");
-            editTextName.requestFocus();
+            binding.name.setError("Full Name is required");
+            binding.name.requestFocus();
+            return;
+        }else if (name.length()>=15){
+            binding.name.setError("Name length is only 15 ");
+            binding.name.requestFocus();
+            return;
+        }
+
+        if(gender.isEmpty()){
+            //binding.gender.setError("Gender is required");
+            binding.gender.requestFocus();
+            return;
+        }
+
+        if(dob.isEmpty()){
+            binding.dob.setError("Date of Birth is required");
+            binding.dob.requestFocus();
             return;
         }
 
         if(phoneNo.isEmpty()){
-            editTextPhoneNo.setError("Phone Number is required");
-            editTextPhoneNo.requestFocus();
+            binding.phoneNo.setError("Phone Number is required");
+            binding.phoneNo.requestFocus();
             return;
         }
 
         if(phoneNo.length() < 10){
-            editTextPhoneNo.setError("Phone Number is required");
-            editTextPhoneNo.requestFocus();
+            binding.phoneNo.setError("Please provide valid Phone Number");
+            binding.phoneNo.requestFocus();
             return;
         }
 
         if(email.isEmpty()){
-            editTextEmail.setError("email is required");
-            editTextEmail.requestFocus();
+            binding.email.setError("email is required");
+            binding.email.requestFocus();
             return;
         }
 
         if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            editTextEmail.setError("Please provide valid Email");
-            editTextEmail.requestFocus();
+            binding.email.setError("Please provide valid Email");
+            binding.email.requestFocus();
             return;
         }
 
         if(password.isEmpty()){
-            editTextPassword.setError("Password is required");
-            editTextPassword.requestFocus();
+            binding.password.setError("Password is required");
+            binding.password.requestFocus();
             return;
         }
 
         if(password.length() < 6){
-            editTextPassword.setError("Min Password length should 6 characters!");
-            editTextPassword.requestFocus();
+            binding.password.setError("Min Password length should 6 characters!");
+            binding.password.requestFocus();
             return;
         }
 
         if(confirmPassword != null && !confirmPassword.equals(password)) {
-            editTextConfirmPassword.setError("Password Doesn't Match");
-            editTextConfirmPassword.requestFocus();
+            binding.confirmPassword.setError("Password Doesn't Match");
+            binding.confirmPassword.requestFocus();
             return;
             }
         progressBar.setVisibility(View.VISIBLE);
