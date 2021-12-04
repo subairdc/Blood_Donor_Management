7a6c3b21c539735e5ce0d9d600704c2b09412c05
subairdc <subairdc@gmail.com>
2021-12-04 21:50:54 +0530

    Add donor validation,update firbase,gender

--- app/src/main/java/com/subairdc/bdma/ui/Fragments/AddDonorFragment.java
+++ app/src/main/java/com/subairdc/bdma/ui/Fragments/AddDonorFragment.java
@@ -1,66 +1,144 @@
 package com.subairdc.bdma.ui.Fragments;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.DatePicker;
+import com.subairdc.bdma.MainActivity;
 import com.subairdc.bdma.R;
+import com.subairdc.bdma.databinding.FragmentAddDonorBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddDonorFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Calendar;
+
+
 public class AddDonorFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    FragmentAddDonorBinding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AddDonorFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddDonorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddDonorFragment newInstance(String param1, String param2) {
-        AddDonorFragment fragment = new AddDonorFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    String gender;
+    DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+        binding = FragmentAddDonorBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentAddDonorBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_donor, container, false);
+        return view;
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        /*Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        binding.ivdob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        month = month+1;
+                        String date = day+"/"+month+"/"+year;
+                        binding.dob.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });*/
+
+        binding.addDonor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = binding.name.getText().toString().trim();
+                int id = binding.rgGender.getCheckedRadioButtonId();
+                String dob = binding.dob.getText().toString().trim();
+                String phoneNo = binding.phoneNo.getText().toString().trim();
+                String email = binding.email.getText().toString().trim();
+
+                if (name.isEmpty()) {
+                    binding.name.setError("Full Name is required");
+                    binding.name.requestFocus();
+                    return;
+                } else if (name.length() >= 20) {
+                    binding.name.setError("Name length is only 15 ");
+                    binding.name.requestFocus();
+                    return;
+                }
+                switch (id) {
+                    case R.id.rbMale:
+                        gender = "Male";
+                        break;
+
+                    case R.id.rbFemale:
+                        gender = "Female";
+                        break;
+
+                    case R.id.rbOther:
+                        gender = "Other";
+                        break;
+                }
+
+                if (dob.isEmpty()) {
+                    binding.dob.setError("Date of Birth is required");
+                    binding.dob.requestFocus();
+                    return;
+                }
+
+                if (phoneNo.isEmpty()) {
+                    binding.phoneNo.setError("Phone Number is required");
+                    binding.phoneNo.requestFocus();
+                    return;
+                }
+
+                if (phoneNo.length() < 10) {
+                    binding.phoneNo.setError("Please provide valid Phone Number");
+                    binding.phoneNo.requestFocus();
+                    return;
+                }
+
+                if (email.isEmpty()) {
+                    binding.email.setError("email is required");
+                    binding.email.requestFocus();
+                    return;
+                }
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    binding.email.setError("Please provide valid Email");
+                    binding.email.requestFocus();
+                    return;
+                }
+
+                binding.progressBar.setVisibility(View.VISIBLE);
+            }
+
+            });
     }
 }
