833f03743949da84bbd89516c05ec78f7b783b9a
subairdc <subairdc@gmail.com>
2022-01-01 15:49:47 +0530

    find donor button search

--- app/src/main/java/com/subairdc/bdma/ui/Fragments/FindDonorFragment.java
+++ app/src/main/java/com/subairdc/bdma/ui/Fragments/FindDonorFragment.java
@@ -1,5 +1,6 @@
 package com.subairdc.bdma.ui.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
@@ -16,6 +18,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.subairdc.bdma.Activities.Donors;
 import com.subairdc.bdma.MyAdapter;
 import com.subairdc.bdma.R;
+import com.subairdc.bdma.databinding.FragmentFindDonorBinding;
 
 
 public class FindDonorFragment extends Fragment {
@@ -23,6 +26,18 @@ public class FindDonorFragment extends Fragment {
     DatabaseReference reference;
     MyAdapter myAdapter;
     RecyclerView recyclerView;
+
+    Button apbutton;
+    Button anbutton;
+    Button bpbutton;
+    Button bnbutton;
+    Button abpbutton;
+    Button abnbutton;
+    Button opbutton;
+    Button onbutton;
+    Button othersbutton;
+    Button allbutton;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,6 +50,23 @@ public class FindDonorFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_find_donor, container, false);
 
+        apbutton = (Button)view.findViewById(R.id.apositive);
+       /* button.setOnClickListener(this);*/
+        anbutton = (Button)view.findViewById(R.id.anegative);
+
+        bpbutton = (Button)view.findViewById(R.id.bpositive);
+        bnbutton = (Button)view.findViewById(R.id.bnegative);
+
+        abpbutton = (Button)view.findViewById(R.id.abpositive);
+        abnbutton = (Button)view.findViewById(R.id.abnegative);
+
+        opbutton = (Button)view.findViewById(R.id.opositive);
+        onbutton = (Button)view.findViewById(R.id.onegative);
+
+        othersbutton = (Button)view.findViewById(R.id.others);
+        allbutton = (Button)view.findViewById(R.id.all);
+
+
         reference = FirebaseDatabase.getInstance().getReference("Donors");
         recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -46,7 +78,109 @@ public class FindDonorFragment extends Fragment {
 
         myAdapter = new MyAdapter(options);
         recyclerView.setAdapter(myAdapter);
+        apbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseRecyclerOptions<Donors> options =
+                        new FirebaseRecyclerOptions.Builder<Donors>()
+                                .setQuery(FirebaseDatabase.getInstance().getReference().child("Donors").orderByChild("bloodGrp").equalTo("A+"), Donors.class)//Case sensitive
+                                .build();
+                myAdapter =new MyAdapter(options);
+                myAdapter.startListening();
+                recyclerView.setAdapter(myAdapter);
+            }
+        });
 
+        anbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseRecyclerOptions<Donors> options =
+                        new FirebaseRecyclerOptions.Builder<Donors>()
+                                .setQuery(FirebaseDatabase.getInstance().getReference().child("Donors").orderByChild("bloodGrp").equalTo("A-"), Donors.class)//Case sensitive
+                                .build();
+                myAdapter =new MyAdapter(options);
+                myAdapter.startListening();
+                recyclerView.setAdapter(myAdapter);
+            }
+        });
+
+        bpbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseRecyclerOptions<Donors> options =
+                        new FirebaseRecyclerOptions.Builder<Donors>()
+                                .setQuery(FirebaseDatabase.getInstance().getReference().child("Donors").orderByChild("bloodGrp").equalTo("B+"), Donors.class)//Case sensitive
+                                .build();
+                myAdapter =new MyAdapter(options);
+                myAdapter.startListening();
+                recyclerView.setAdapter(myAdapter);
+            }
+        });
+
+        bnbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseRecyclerOptions<Donors> options =
+                        new FirebaseRecyclerOptions.Builder<Donors>()
+                                .setQuery(FirebaseDatabase.getInstance().getReference().child("Donors").orderByChild("bloodGrp").equalTo("B-"), Donors.class)//Case sensitive
+                                .build();
+                myAdapter =new MyAdapter(options);
+                myAdapter.startListening();
+                recyclerView.setAdapter(myAdapter);
+            }
+        });
+
+        opbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseRecyclerOptions<Donors> options =
+                        new FirebaseRecyclerOptions.Builder<Donors>()
+                                .setQuery(FirebaseDatabase.getInstance().getReference().child("Donors").orderByChild("bloodGrp").equalTo("O+"), Donors.class)//Case sensitive
+                                .build();
+                myAdapter =new MyAdapter(options);
+                myAdapter.startListening();
+                recyclerView.setAdapter(myAdapter);
+            }
+        });
+
+        onbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseRecyclerOptions<Donors> options =
+                        new FirebaseRecyclerOptions.Builder<Donors>()
+                                .setQuery(FirebaseDatabase.getInstance().getReference().child("Donors").orderByChild("bloodGrp").equalTo("O-"), Donors.class)//Case sensitive
+                                .build();
+                myAdapter =new MyAdapter(options);
+                myAdapter.startListening();
+                recyclerView.setAdapter(myAdapter);
+            }
+        });
+
+        allbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseRecyclerOptions<Donors> options =
+                        new FirebaseRecyclerOptions.Builder<Donors>()
+                                .setQuery(FirebaseDatabase.getInstance().getReference().child("Donors").orderByChild("bloodGrp"), Donors.class)//Case sensitive
+                                .build();
+                myAdapter =new MyAdapter(options);
+                myAdapter.startListening();
+                recyclerView.setAdapter(myAdapter);
+            }
+        });
+
+        othersbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseRecyclerOptions<Donors> options =
+                        new FirebaseRecyclerOptions.Builder<Donors>()
+                                .setQuery(FirebaseDatabase.getInstance().getReference().child("Donors").orderByChild("bloodGrp"), Donors.class)//Case sensitive
+                                .build();
+                myAdapter =new MyAdapter(options);
+                myAdapter.startListening();
+                recyclerView.setAdapter(myAdapter);
+            }
+        });
 
 
 
@@ -64,4 +198,29 @@ public class FindDonorFragment extends Fragment {
         super.onStop();
         myAdapter.stopListening();
     }
+/*
+    private void bloodGrpSearch(String str){
+        FirebaseRecyclerOptions<Donors> options =
+                new FirebaseRecyclerOptions.Builder<Donors>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Donors").orderByChild("bloodGrp").equalTo(str), Donors.class)//Case sensitive
+                        .build();
+        myAdapter =new MyAdapter(options);
+        myAdapter.startListening();
+        recyclerView.setAdapter(myAdapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id=view.getId();
+
+        switch (id){
+            case R.id.opositive:
+                String str = "O+";
+                bloodGrpSearch(str);
+                break;
+            case R.id.bpositive:
+                bloodGrpSearch("B+");
+                break;
+        }
+    }*/
 }
